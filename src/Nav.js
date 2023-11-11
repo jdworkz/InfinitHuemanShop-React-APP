feecdad8e5412236fdfa8dfc00c64a1b67615df8
jdworkz <louisjhondy@gmail.com>
2023-11-11 18:20:34 -0500

    Remove some files not needed

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+function Nav(){
+    return(
+        <div className="APP">
+        <ul>
+            <li>Home</li>
+            <li>Contacts</li>
+            <li>Shop</li>
+            <p>hi yall</p>
+           
+        </ul>
+        </div>
+    )
+}
+
+export default Nav;
